908849548519e0f7fd14aeac9267a96fdd71ef76
Natanael Rodrigues <natanaelrodrigues@gmail.com>
2017-09-01 12:00:28 -0300

    Alteracao na url

--- simplemooc/urls.py
+++ simplemooc/urls.py
@@ -17,7 +17,8 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 
+
 urlpatterns = [
-    url(r'^$', include('simplemooc.core.urls')),
+    url(r'^', include('simplemooc.core.urls')),
     url(r'^admin/', admin.site.urls),
 ]
