1169b756a9ff9df09fdcacd17a2848eb373910f7
Natanael Rodrigues <natanaelrodrigues@gmail.com>
2017-09-07 10:56:58 -0300

    Aula 10

--- simplemooc/core/views.py
+++ simplemooc/core/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-	return render(request, 'home.html', {'usuario':'Natanael'})
+	return render(request, 'home.html')
+
+def contact(request):
+	return render(request, 'contact.html')
